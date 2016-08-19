532775c18498f730724b69d9cdc6b641bdef4107
Long Hoang <long@mindworker.de>
2016-08-20 01:01:42 +0800

    Add CORS

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/receipts', function (req, res) {
   console.log("POST receipts");
 
